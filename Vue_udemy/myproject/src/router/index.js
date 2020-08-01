c3751dbd8e5c6fc49b8105251b7d91d7a5d0c3ac
linh59 <1059012@itec.hcmus.edu.vn>
2020-08-01 08:59:26 +0700

    update

--- Vue_udemy/myproject/src/router/index.js
+++ Vue_udemy/myproject/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/Server/HelloWorld'
 import QuotePage from '@/views/quotePage'
+import FormPage from '@/views/FormPage'
+import DirectivePage from '@/views/directivePage'
 
 Vue.use(Router)
 
@@ -17,6 +19,16 @@ export default new Router({
       path: '/QuotePage',
       name: 'QuotePage',
       component: QuotePage
+    },
+    {
+      path: '/FormPage',
+      name: 'FormPage',
+      component: FormPage
+    },
+    {
+      path: '/DirectivePage',
+      name: 'DirectivePage',
+      component: DirectivePage
     },
   ]
 })
---
+++ Vue_udemy/myproject/src/views/directivePage.vue
@@ -0,0 +1,45 @@
+<template>
+ <div class="row">
+     <div class="col-12">
+         <h1>Directive</h1>
+         <p v-text="'Some text'"></p>
+     </div>
+  </div>
+</template>
+
+<script>
+  import Switch from '../components/Server/Switch';
+
+  export default {
+    name: 'formPage',
+    data: function() {
+      return {
+        userData:{
+          email: '',
+          password: '',
+          age: 20
+        },
+        measage:'A text message',
+        sendMail: [],
+        gender: 'Male',
+        priorities: ['High','Medium','Low'],
+        selectedPriority: 'Low',
+        dataSwitch: true,
+        isSubmitted: false
+      }
+    },
+    methods: {
+      submitted() {
+        this.isSubmitted = true;
+      }
+    },
+    components: {
+      'switch-form': Switch
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
+
